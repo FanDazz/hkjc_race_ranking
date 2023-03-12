16f01481b9e6bb95c786c3319056b2f921764809
georgeDuSH <georgedush@163.com>
2023-03-12 17:40:05 +0800

    modeling

--- README.md
+++ README.md
@@ -9,5 +9,9 @@ See `./01_web_scrapper.ipynb`
 ## 3 Intro and Baselines
 See `./02_intro_n_baseline.ipynb`
 
+## 4 Modeling
+Initial trails is published, see `./04_modeling.ipynb`.
+- Results: https://docs.google.com/spreadsheets/d/1yV3CTazxirW9HeyC6Mmy0n6ybgR68nZEDlycqWVyGxA/edit#gid=0
+
 ## TODOs
 Check [Notion](https://abaft-taker-831.notion.site/Horse-Racing-aea5f5b3577b4402bbfd9ec5148ed36e)
--- horse/process.py
+++ horse/process.py
@@ -18,3 +18,19 @@ def AveragePrecision(input, target):
     merge_df = input.merge(target, on='race_key', how='left')
     
     return (merge_df.iloc[:, 1]==merge_df.iloc[:, 2]).sum()/merge_df.shape[0]
+
+
+remove_odds = lambda x: [col for col in x if col != 'win_odds']
+
+def comupte_champ(df, model, x_cols, kind='clf'):
+    X = df[x_cols]
+    result = df[['race_key', 'dr']]
+
+    if kind=='clf':
+        result['win'] = model.predict_proba(X)[:, 1]
+    elif kind=='reg':
+        result['win'] = model.predict(X)
+
+    return result.groupby(['race_key']) \
+                .apply(lambda x: x[x['win']==x['win'].max()]) \
+                .reset_index(drop=True)[['race_key', 'dr']]
